75a6ee0b821527830f154e7a558b6cca5abac84e
WhenLifeHandsYouLemons <You'veGotToMakeLemonade@users.noreply.github.com>
2020-08-28 17:14:48 +0800

    Update Rubik's Cube Timer.py
    ------------------------------
    Added
    - App name background
    ------------------------------
    Removed
    - Time display background
    ------------------------------
    Changed
    - Time display font, position
    ------------------------------

--- Rubik's Cube Timer.py
+++ Rubik's Cube Timer.py
@@ -1,8 +1,8 @@
 """
 high level support for doing this and that
 """
-import pygame
 import time
+import pygame
 pygame.init()
 
 #This sets the size of the window.
@@ -10,7 +10,6 @@ WIN = pygame.display.set_mode((1250, 680))
 pygame.display.set_caption("Rubik's Cube Timer")
 
 bgColor = 0, 0, 0
-
 
 timer_start = 0
 if timer_start == 0:
@@ -25,15 +24,15 @@ timer_button_height = 100
 timer_button_width = 250
 timer_button_colour = 255, 255, 255
 
-timer_display_font = 'C:/Windows/Fonts/Corbel.ttf'
+timer_display_font = 'C:/Windows/Fonts/Arial.ttf'
 timer_display_size = 150
 timer_display_fg = 255, 255, 255
-timer_display_bg = 0, 0, 0
-timer_display_x = timer_button_x
+timer_display_bg = None
+timer_display_x = timer_button_x - timer_button_x / 10
 timer_display_y = 200
 
-def timer_function():
 
+def timer_function():
     timer_font = pygame.font.Font(timer_display_font, timer_display_size)
     total_time = str(round(time.time() - startTime, 2))
     timer_display = timer_font.render(total_time, True, timer_display_fg, timer_display_bg)
@@ -49,6 +48,8 @@ clock = pygame.time.Clock()
 
 def game_window_style():
     WIN.fill(bgColor)
+    pygame.draw.rect(WIN, (200, 200, 200), (0, 0, 1250, timer_button_height))
+    #d
 
 
 RUNNING_WINDOW = True
