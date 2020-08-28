55aa39e15555cfdb26194f255fa22aecf31ccd8b
WhenLifeHandsYouLemons <You'veGotToMakeLemonade@users.noreply.github.com>
2020-08-28 11:19:15 +0800

    Adding timing function
    ------------------------------
    Added
    - Timing function (in progress)
    ------------------------------
    Removed
    ------------------------------
    Changed
    - Start/Stop button colour, position & size
    ------------------------------

--- Rubik's Cube Timer.py
+++ Rubik's Cube Timer.py
@@ -13,18 +13,28 @@ bgColor = 0, 0, 0
 
 waiting_time = 0
 
-timer_button_x = 0
-timer_button_y = 0
+timer_button_x = 700
+timer_button_y = 450
 timer_button_height = 100
-timer_button_width = 100
+timer_button_width = 250
+timer_colour = 255, 255, 255
+
+def timer_function():
+    startTime = time.time()
+    continuous_time = str(time.time() - startTime)
+    timer_font = pygame.font.Font('C:/Users/2005s/Documents/Fonts/Sheandy-dqo7.ttf', 32)
+    timer_display = timer_font.render(continuous_time, True, (255, 255, 255), (0, 0, 0))
+    textRect = timer_display.get_rect()
+    textRect.center = (2 // 2, 2 // 2)
 
 def start_timer():
-    pygame.draw.rect(WIN, (255, 255, 255), (timer_button_x, timer_button_y, timer_button_width, timer_button_height))
+    pygame.draw.rect(WIN, (timer_colour), (timer_button_x, timer_button_y, timer_button_width, timer_button_height))
 
 clock = pygame.time.Clock()
 
 def game_window_style():
     WIN.fill(bgColor)
+
 
 RUNNING_WINDOW = True
 
@@ -43,6 +53,7 @@ while RUNNING_WINDOW:
 
     game_window_style()
     start_timer()
+    timer_function()
     pygame.display.update()
 
 
