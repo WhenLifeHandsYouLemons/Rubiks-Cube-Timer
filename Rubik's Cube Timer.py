a3dfe1bc14fee6a841bac044d074a75b80ca25a7
WhenLifeHandsYouLemons <You'veGotToMakeLemonade@users.noreply.github.com>
2020-08-28 12:00:43 +0800

    Adding Timing Function
    ------------------------------
    Added
    - Time display
    - Get current time (to show elapsed time for timer)
    ------------------------------
    Removed
    - Previous time display code
    ------------------------------
    Changed
    - Time display font, size, position
    - Variable naming
    ------------------------------

--- Rubik's Cube Timer.py
+++ Rubik's Cube Timer.py
@@ -11,24 +11,39 @@ pygame.display.set_caption("Rubik's Cube Timer")
 
 bgColor = 0, 0, 0
 
+
+timer_start = 0
+if timer_start == 0:
+    startTime = time.time()
+    timer_start = 1
+
 waiting_time = 0
 
 timer_button_x = 700
 timer_button_y = 450
 timer_button_height = 100
 timer_button_width = 250
-timer_colour = 255, 255, 255
+timer_button_colour = 255, 255, 255
+
+timer_display_font = 'C:/Windows/Fonts/Corbel.ttf'
+timer_display_size = 150
+timer_display_fg = 255, 255, 255
+timer_display_bg = 0, 0, 0
+timer_display_x = timer_button_x
+timer_display_y = 200
 
 def timer_function():
-    startTime = time.time()
-    continuous_time = str(time.time() - startTime)
-    timer_font = pygame.font.Font('C:/Users/2005s/Documents/Fonts/Sheandy-dqo7.ttf', 32)
-    timer_display = timer_font.render(continuous_time, True, (255, 255, 255), (0, 0, 0))
+
+    timer_font = pygame.font.Font(timer_display_font, timer_display_size)
+    total_time = str(round(time.time() - startTime, 2))
+    timer_display = timer_font.render(total_time, True, timer_display_fg, timer_display_bg)
     textRect = timer_display.get_rect()
-    textRect.center = (2 // 2, 2 // 2)
+    textRect.left = timer_display_x
+    textRect.top = timer_display_y
+    WIN.blit(timer_display, textRect)
 
 def start_timer():
-    pygame.draw.rect(WIN, (timer_colour), (timer_button_x, timer_button_y, timer_button_width, timer_button_height))
+    pygame.draw.rect(WIN, (timer_button_colour), (timer_button_x, timer_button_y, timer_button_width, timer_button_height))
 
 clock = pygame.time.Clock()
 
