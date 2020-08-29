8ae82150e8821d9060824d6e54581675a7b51ab7
WhenLifeHandsYouLemons <You'veGotToMakeLemonade@users.noreply.github.com>
2020-08-29 21:11:32 +0800

    Added Space Bar Support
    ------------------------------
    Added
    - Space bar support (to start timer)
    - Time display of "0.000" before starting
    - Timer start check (using a list)
    ------------------------------
    Removed
    - Waiting time variable
    ------------------------------
    Changed
    - Function "start_timer" to check for more than just space bar and button click
    - Formatting of code
    ------------------------------

--- Rubik's Cube Timer.py
+++ Rubik's Cube Timer.py
@@ -12,11 +12,6 @@ pygame.display.set_caption("Rubik's Cube Timer")
 bgColor = 0, 0, 0
 
 timer_start = 0
-if timer_start == 0:
-    startTime = time.time()
-    timer_start = 1
-
-waiting_time = 0
 
 timer_button_x = 700
 timer_button_y = 450
@@ -24,8 +19,10 @@ timer_button_height = 100
 timer_button_width = 250
 timer_button_colour = 255, 255, 255
 timer_button_colour_selected = 150, 150, 150
+button_click_check = [0]
+startTime = time.time()
 
-timer_display_font = 'C:/Windows/Fonts/Arial.ttf'
+timer_display_font = "C:/Windows/Fonts/Arial.ttf"
 timer_display_size = 150
 timer_display_fg = 255, 255, 255
 timer_display_bg = None
@@ -34,20 +31,47 @@ timer_display_y = 200
 
 
 def timer_function():
-    timer_font = pygame.font.Font(timer_display_font, timer_display_size)
-    total_time = str(round(time.time() - startTime, 3))
-    timer_display = timer_font.render(total_time, True, timer_display_fg, timer_display_bg)
-    textRect = timer_display.get_rect()
-    textRect.left = timer_display_x
-    textRect.top = timer_display_y
-    WIN.blit(timer_display, textRect)
+#    time.sleep(0.5)
+    while button_click_check[0] == 1:
+#        if keys[pygame.K_SPACE]:
+#            button_click_check.remove(1)
+#            button_click_check.append(0)
+#        if mouse[0] > timer_button_x and mouse[0] < timer_button_x + timer_button_width and mouse[1] > timer_button_y and mouse[1] < timer_button_y + timer_button_height and pygame.mouse.get_pressed()[0]:
+#            button_click_check.remove(1)
+#            button_click_check.append(0)
+        timer_font = pygame.font.Font(timer_display_font, timer_display_size)
+        total_time = str(round(time.time() - startTime, 3))
+        timer_display = timer_font.render(total_time, True, timer_display_fg, timer_display_bg)
+        textRect = timer_display.get_rect()
+        textRect.left = timer_display_x
+        textRect.top = timer_display_y
+        WIN.blit(timer_display, textRect)
+        pygame.display.update()
+        game_window_style()
 
 def start_timer():
-    mouse = pygame.mouse.get_pos()
-    if mouse[0] > timer_button_x and mouse[0] < timer_button_x + timer_button_width and mouse[1] > timer_button_y and mouse[1] < timer_button_y + timer_button_height and pygame.mouse.get_pressed()[0]:
+    if button_click_check[0] == 1:
+        timer_function()
+    elif keys[pygame.K_SPACE] and button_click_check[0] == 0:
+        button_click_check.append(1)
+        button_click_check.remove(0)
+        startTime = time.time()
+        print(button_click_check)
+        timer_function()
+    elif mouse[0] > timer_button_x and mouse[0] < timer_button_x + timer_button_width and mouse[1] > timer_button_y and mouse[1] < timer_button_y + timer_button_height and pygame.mouse.get_pressed()[0] and button_click_check[0] == 0:
         pygame.draw.rect(WIN, (timer_button_colour_selected), (timer_button_x, timer_button_y, timer_button_width, timer_button_height))
         print("Clicked!")
+        button_click_check.append(1)
+        button_click_check.remove(0)
+        startTime = time.time()
+        timer_function()
     else:
+        timer_font = pygame.font.Font(timer_display_font, timer_display_size)
+        timer_display = timer_font.render("0.000", True, timer_display_fg, timer_display_bg)
+        textRect = timer_display.get_rect()
+        textRect.left = timer_display_x
+        textRect.top = timer_display_y
+        WIN.blit(timer_display, textRect)
         pygame.draw.rect(WIN, (timer_button_colour), (timer_button_x, timer_button_y, timer_button_width, timer_button_height))
 
 clock = pygame.time.Clock()
@@ -68,14 +92,10 @@ while RUNNING_WINDOW:
             RUNNING_WINDOW = False
 
     keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_SPACE]:
-        
-        waiting_time = waiting_time + 0.001
+    mouse = pygame.mouse.get_pos()
 
     game_window_style()
     start_timer()
-    timer_function()
     pygame.display.update()
 
 
